1d41b3665025cc7b3dd15c174c9f461f08359fe4
EC2 Default User <ec2-user@ip-172-31-26-6.ap-northeast-1.compute.internal>
2020-08-25 06:57:23 +0000

    udemy no.119 finish

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,8 +7,27 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
+  end
+  
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      flash[:alert] = 'Save Error!'
+      # render 
+    end
   end
 
   def edit
   end
+  
+  private
+    def question_params
+      # byebug
+      params.require(:question).permit(:name, :title, :content)
+      # params.require:question].permit[:name, :title, :content]
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # get 'questions/edit'
   root 'questions#index'
   resources :questions
-  
+  # testtext
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
